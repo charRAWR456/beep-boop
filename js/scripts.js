9d0ff086f895eb3ad585d9ba2ff20ce26784afde
Nick Pittak <nicpitta@gmail.com>
2018-01-19 10:32:09 -0800

    add html user input box and js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+  $("form#beepBoop").submit(function(event) {
+  event.preventDefault();
+    userArray = []
+    var userInput = $("input#userNumber").val();
+    for (var i = 0; i <= userInput; i++){
+      userArray[i] = i;
+    }
+
+    userArray.forEach(function(number){
+      if (number == 0) {
+        number = "beep";
+      }
+     });
+
+
+    console.log(userInput);
+    var result = "Your result: " + userArray;
+
+
+
+    $("#output").text(result);
+  });
 });
