5df805a12efec6461b102d0bae21584e26e7ed75
Tyler Wickline <twick00@gmail.com>
2018-01-22 15:25:50 -0800

    condense code and remove code that is not needed

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,7 @@ $(document).ready(function() {
     if (userNumber < 0) {
       alert("Please enter a positive number!");
     }
-    //for (var i = 0; o < userNumber.toString().length; i++)
-    //if (userArray[o].toString()[i] === 1||0)
-var o = 0;
+
       for(var number = 0; number < userArray.length; number += 1){
         for (var digit = 0; digit < userArray.toString().length; digit += 1){
           if (userArray[number].toString()[digit] === "0"){
