7154d40a81241a865652fa2ce6d38d3e73e75ca0
Tyler Wickline <twick00@gmail.com>
2018-01-22 14:49:16 -0800

    add Javascript that adds Beep to 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
 
   $("form#beepBoop").submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
     userArray = []
-    var userInput = $("input#userNumber").val();
-    for (var i = 0; i <= userInput; i++){
+    var userNumber = $("input#userNumber").val();
+    var userName = $("input#userName").val();
+
+    for (var i = 0; i <= userNumber; i++){
       userArray[i] = i;
     }
-    if (i = 0) {
-        i = "beep";
+    if (userName == 0) {
+      alert("Please Enter your Name!");
+    }
+    if (userNumber == 0) {
+      alert("Please Enter a Number!");
+    }
+    if (userNumber < 0) {
+      alert("Please enter a positive number!");
+    }
+  // stringArray = $(".sentance").text().split("");
+    for(var o = 0; o < userNumber.length; o += 1){
+      if (userArray[o] === 0){
+        userArray[o] = "Beep!";
       }
-    
-
-
-    console.log(userInput);
-    var result = "Your result: " + userArray;
-
-
+      else if(userArray[o] % 2 === 0){
+         userArray[o] = "Boop!"
+      }
+    }
+    newArray = userArray.join(", ")
+    var result = "Your result: " + newArray;
 
     $("#output").text(result);
   });
